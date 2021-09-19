1118199e96c64a56ee7336e522acb8c1340096ef
Gustavo Sorati <gustavo.sorati@outlook.com>
2021-09-19 10:01:49 -0300

    Integração com API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
-import styles from './home.module.scss';
-
 import Head from 'next/head';
+import { GetServerSideProps, GetStaticProps } from 'next';
+
 import { SubscribeButton } from '../components/SubscribeButton';
 
-export default function Home() {
+import styles from './home.module.scss';
+import { stripe } from '../services/stripe';
+
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -16,9 +26,12 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            }).format(product.amount)} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"  />
@@ -26,3 +39,46 @@ export default function Home() {
     </>
   )
 }
+
+// SSR
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1J0bACKKZhmsP0BkyDitfyog', {
+//     expand: ['product'] // tem acesso a todas informações do produto
+//   });
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-US', {
+//       style: 'currency',
+//       currency: 'USD'
+//     }).format(price.unit_amount / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
+
+// SSG
+export const getStaticProps: GetStaticProps = async () => {
+    const price = await stripe.prices.retrieve('price_1J0bACKKZhmsP0BkyDitfyog', {
+      expand: ['product'] // tem acesso a todas informações do produto
+    });
+
+    const product = {
+      priceId: price.id,
+      amount: new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(price.unit_amount / 100)
+    }
+
+    return {
+      props: {
+        product
+      },
+      revalidate: 60 * 60 * 24// 24 horas
+    }
+  }
